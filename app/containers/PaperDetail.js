5d2d95c2fd6c2c23f99f9ee96045db5363dcc311
VarKamiu <2558823515@qq.com>
2020-05-25 08:52:32 +0800

    修正

--- app/containers/PaperDetail.js
+++ app/containers/PaperDetail.js
@@ -28,51 +28,51 @@ class PaperDetail extends Component {
     const { currentMessage } = this.props
     const { content, time, sender, title, type } = currentMessage
     return (
-      <BasicPage  >
-        <View style={{ height: 1000 }}>
-          <ScrollView
-            style={{ flex: 1 }}
-            automaticallyAdjustContentInsets={false}
-            showsHorizontalScrollIndicator={false}
-            showsVerticalScrollIndicator={false}
-          >
-            <WingBlank size="md" >
-              <WhiteSpace size="md" />
-              <Text style={{ fontFamily: "STHeiti", fontSize: 25, color: "black" }}>{title}</Text>
-              <WhiteSpace size="sm" />
-              <Text style={{ color: "grey" }}>{`${sender}  ${type}  ${time}`}</Text>
-              <WhiteSpace size="lg" />
-              <View style={{ backgroundColor: "white", paddingLeft: 5, paddingRight: 5, paddingTop: 10, paddingBottom: 10 }}>
-                {
-                  content.map((item, index) => {
-                    const { type: ItemType, value } = item
-                    if (ItemType === "image") {
-                      return (
-                        <Flex direction="column"
-                          key={`${ItemType}${index}`}
-                        >
-                          <Image
-                          style={{width:200, height:112}}
-                          source={require("../images/tim.jpg")}
-                          />
-                        </Flex>
-                      )
-                    }
+
+      <ScrollView
+        style={{ flex: 1 }}
+        automaticallyAdjustContentInsets={false}
+        showsHorizontalScrollIndicator={false}
+        showsVerticalScrollIndicator={false}
+      >
+        <BasicPage  >
+          <WingBlank size="md" >
+            <WhiteSpace size="md" />
+            <Text style={{ fontFamily: "STHeiti", fontSize: 25, color: "black" }}>{title}</Text>
+            <WhiteSpace size="sm" />
+            <Text style={{ color: "grey" }}>{`${sender}  ${type}  ${time}`}</Text>
+            <WhiteSpace size="lg" />
+            <View style={{ backgroundColor: "white", paddingLeft: 5, paddingRight: 5, paddingTop: 10, paddingBottom: 10 }}>
+              {
+                content.map((item, index) => {
+                  const { type: ItemType, value } = item
+                  if (ItemType === "image") {
                     return (
-                      <Text
+                      <Flex direction="column"
                         key={`${ItemType}${index}`}
-                        style={{ color: "black", fontSize: 16, backgroundColor: "white", }}
                       >
-                        {value}
-                      </Text>
+                        <Image
+                          style={{ width: 200, height: 112 }}
+                          source={require("../images/tim.jpg")}
+                        />
+                      </Flex>
                     )
-                  })
-                }
-              </View>
-            </WingBlank>
-          </ScrollView>
-        </View>
-      </BasicPage>
+                  }
+                  return (
+                    <Text
+                      key={`${ItemType}${index}`}
+                      style={{ color: "black", fontSize: 16, backgroundColor: "white", }}
+                    >
+                      {value}
+                    </Text>
+                  )
+                })
+              }
+            </View>
+          </WingBlank>
+        </BasicPage>
+      </ScrollView>
+
     )
   }
 }
